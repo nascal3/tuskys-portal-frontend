1f931e401dac1d0a224a23d2d4e5ceb752249a4f
nascal3 <nascal3@gmail.com>
2019-01-17 15:56:10 +0300

    created sidebar navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-
+import {SidebarComponent} from "./sidebar/sidebar.component";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
