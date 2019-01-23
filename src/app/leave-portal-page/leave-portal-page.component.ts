75ca79e1c7228b6a1075407cab2e399abcb5337c
nascal3 <nascal3@gmail.com>
2019-01-23 14:17:29 +0300

    corrected a mozila styles display issue and solved full page height issue

--- src/app/leave-portal-page/leave-portal-page.component.ts
+++ src/app/leave-portal-page/leave-portal-page.component.ts
@@ -10,24 +10,24 @@ export class LeavePortalPageComponent implements OnInit {
 
   leaveSates: any[] = [];
   tableData = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-    {position: 11, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 12, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 13, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 14, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 15, name: 'Boron', weight: 10.811, symbol: 'B'},
+    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', col2: 1.0079, symbol2: 'H'},
+    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', col2: 1.0079, symbol2: 'H'},
+    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', col2: 1.0079, symbol2: 'H'},
+    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', col2: 1.0079, symbol2: 'H'},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B', col2: 1.0079, symbol2: 'H'},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C', col2: 1.0079, symbol2: 'H'},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N', col2: 1.0079, symbol2: 'H'},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O', col2: 1.0079, symbol2: 'H'},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F', col2: 1.0079, symbol2: 'H'},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne', col2: 1.0079, symbol2: 'H'},
+    {position: 11, name: 'Hydrogen', weight: 1.0079, symbol: 'H', col2: 1.0079, symbol2: 'H'},
+    {position: 12, name: 'Helium', weight: 4.0026, symbol: 'He', col2: 1.0079, symbol2: 'H'},
+    {position: 13, name: 'Lithium', weight: 6.941, symbol: 'Li', col2: 1.0079, symbol2: 'H'},
+    {position: 14, name: 'Beryllium', weight: 9.0122, symbol: 'Be', col2: 1.0079, symbol2: 'H'},
+    {position: 15, name: 'Boron', weight: 10.811, symbol: 'B', col2: 1.0079, symbol2: 'H'},
   ];
-  dataSource = new MatTableDataSource<any>(this.tableData);
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource(this.tableData);
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','col2', 'symbol2'];
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
