afc090d9af340dfe4a6304c0e5b226e7cb60bee5
nascal3 <nascal3@gmail.com>
2019-01-24 16:27:27 +0300

    added function to read router state from store and a few code refactor

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Select} from "@ngxs/store";
+import {PageState} from "../state/page.state";
+import {Observable} from "rxjs";
+import {PageModel} from "../models/page.model";
 
 @Component({
   selector: 'app-sidebar',
@@ -8,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  activePage: PageModel;
+  travelApp: boolean = false;
+  leave: boolean = false;
+  @Select(PageState.getActiveRoute) page$: Observable<PageModel>;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.page$.subscribe(res => {
+      this.activePage = res;
+      this.selectTabHighlight(this.activePage);
+    });
+  }
+
+  selectTabHighlight(route) {
+    if (route === '/travel') {
+      this.travelApp = true;
+    } else if (route === '/leave') {
+      this.leave = true;
+    }
+
   }
 
 }
